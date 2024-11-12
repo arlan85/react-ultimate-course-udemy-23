0f7b19366bd8488b9d9d3d43f51a7138f1c3028c
Arlan Galvez <galvezalonso@gmail.com>
2024-11-12 18:50:14 -0500

    07-86: calculating statistics and derived state

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -27,7 +27,7 @@ export default function App() {
      <Logo/>
      <Form onAddItems={handleAddItem}/>
      <PackingList items={items} onDeleteItem={handleDeleteItem} onhandlePacked={handlePackedItem}/>
-     <Stats/>
+     <Stats items={items}/>
     </div>
   )
 }
@@ -45,7 +45,6 @@ function Form({onAddItems}){
     e.preventDefault()
     if(!description) return
     const newItem = {description, quantity, packed: false, id: Date.now()}
-    console.log(newItem)
     
     onAddItems(newItem)
     setDescription('')
@@ -86,10 +85,24 @@ function Item({item, onDeleteItem, onhandlePacked}){
   )
 }
 
-function Stats(){
+function Stats({items}){
+  if(!items.length) 
+  return ( 
+    <p className="stats">
+      <em>Start adding some items to your packing list 🚀</em>
+    </p>
+  )
+  const numItems = items.length
+  const packedItems = items.filter(item => item.packed).length
+  const percentage = Math.round((packedItems / numItems) * 100) 
+  
   return (
     <footer className="stats">
-      <em>🧳 You have X items on your list, and you already packed W (Z%)</em>
+       {percentage === 100 ?
+       <em >You got everything ready to go ✈️!</em>
+      :<em>
+        🧳 You have {numItems} items on your list, and you already packed {packedItems} ({percentage}%)</em>
+}
     </footer>
   )
 }

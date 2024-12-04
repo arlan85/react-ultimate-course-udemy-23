1062899169e0724d4b20166a3a6711af9d67a76e
Arlan Galvez <galvezalonso@gmail.com>
2024-12-03 19:24:55 -0500

    10-119: reusability improved with props

--- 10-use-popcorn/src/components/StartRating.js
+++ 10-use-popcorn/src/components/StartRating.js
@@ -10,56 +10,76 @@ const ratingStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
+export default function StartRating({
+  maxRate = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.6}px`,
+  };
 
-const starStyle = {
-  // color: "#FFD700",
-  width: "24px",
-  height: "24px",
-  cursor: "pointer",
-  display: "block",
-};
-
-export default function StartRating({ maxRate = 5 }) {
-  const [rating, setRating] = useState(0);
-  const [tempRate, setTempRate] = useState(0)
-
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRate, setTempRate] = useState(0);
 
   const handleRating = (rating) => {
     setRating(rating);
+    onSetRating(rating);
   };
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={ratingStyle}>
         {Array.from({ length: maxRate }, (_, index) => (
           <Star
             key={index}
-            full={tempRate? tempRate>=index+1: index + 1 <= rating}
+            full={tempRate ? tempRate >= index + 1 : index + 1 <= rating}
             onRate={() => handleRating(index + 1)}
-            onHoverIn={()=>setTempRate(index+1)}
+            onHoverIn={() => setTempRate(index + 1)}
             onHoverOut={() => setTempRate(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRate|| rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRate
+          ? messages[tempRate ? tempRate - 1 : rating - 1]
+          : tempRate || rating || ""}
+      </p>
     </div>
   );
 }
 
-function Star({ full, onRate, onHoverIn, onHoverOut }) {
+function Star({ full, onRate, onHoverIn, onHoverOut, size, color }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    cursor: "pointer",
+    display: "block",
+  };
+
   return (
-    <span role="button" style={starStyle} onClick={onRate} 
-    onMouseEnter={onHoverIn} onMouseLeave={onHoverOut}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -68,7 +88,7 @@ function Star({ full, onRate, onHoverIn, onHoverOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- 10-use-popcorn/src/index.js
+++ 10-use-popcorn/src/index.js
@@ -4,11 +4,27 @@ import StartRating from './components/StartRating';
 // import './index.css';
 // import App from './App';
 
+function Test (){
+  const [rating, setRating] = React.useState(0)
+
+  return (
+    <div>
+      <StartRating maxRate={10}  color='blue' size={30} defaultRating={rating} 
+      onSetRating={setRating}/>
+      <p>This movie was rated {rating} stars</p>
+    </div>
+  )
+
+  }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating />
+    <StartRating messages={
+      ["Terrible", "Bad", "OK", "Good", "Amazing"]
+    }/>
+    <StartRating size={20} color='red' className="test" defaultRating={3}/>
+    <Test></Test>
   </React.StrictMode>
 );
 

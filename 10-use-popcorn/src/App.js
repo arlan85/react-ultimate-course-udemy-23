f39247510c84c8bd511fca245a023d483c896423
Arlan Galvez <galvezalonso@gmail.com>
2025-01-20 18:56:40 -0500

    12-153: adding effect, change page name

--- 10-use-popcorn/src/App.js
+++ 10-use-popcorn/src/App.js
@@ -1,51 +1,5 @@
 import { useEffect, useState } from "react";
 import StartRating from "./components/StartRating";
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
 
 const KEY = "710b0dfa";
 const apiBaseUrl = `http://www.omdbapi.com/?apikey=${KEY}`;
@@ -156,10 +110,9 @@ function WatchedSummary({ watched }) {
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   // const avgRuntime = average(watched.map((movie) => movie.runtime), 0);
   // runtime should not be an average, it should be the total watched time from all movies watched
-  const avgRuntime = 
-    watched.map((movie) => movie.runtime)
-    .reduce((acc, cur) => acc + cur, 0)
-  
+  const avgRuntime = watched
+    .map((movie) => movie.runtime)
+    .reduce((acc, cur) => acc + cur, 0);
 
   return (
     <div className="summary">
@@ -190,7 +143,11 @@ function WatchedMovieList({ watched, onDeletWatched }) {
   return (
     <ul className="list">
       {watched.map((movie) => (
-        <WatchedMovie key={movie.imdbID} movie={movie} onDeletWatched={onDeletWatched}></WatchedMovie>
+        <WatchedMovie
+          key={movie.imdbID}
+          movie={movie}
+          onDeletWatched={onDeletWatched}
+        ></WatchedMovie>
       ))}
     </ul>
   );
@@ -214,7 +171,10 @@ function WatchedMovie({ movie, onDeletWatched }) {
           <span>⏳</span>
           <span>{movie.runtime} min</span>
         </p>
-        <button className="btn-delete" onClick={() => onDeletWatched(movie.imdbID)}>
+        <button
+          className="btn-delete"
+          onClick={() => onDeletWatched(movie.imdbID)}
+        >
           X
         </button>
       </div>
@@ -229,6 +189,7 @@ function Main({ children }) {
 function Loader() {
   return <p className="loader">Loading...</p>;
 }
+
 function ErrorMessage({ message }) {
   return (
     <p className="error">
@@ -244,7 +205,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [userRating, setUserRating] = useState(0);
 
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
-  
+
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
   )?.userRating;
@@ -296,6 +257,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       getMovieDetails();
     },
     [selectedId]
+  );
+
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+      
+    },
+    [title]
   );
 
   return (
@@ -473,7 +443,10 @@ export default function App() {
           ) : (
             <>
               <WatchedSummary watched={watched} />
-              <WatchedMovieList watched={watched} onDeletWatched={handleDeleteWatched} />
+              <WatchedMovieList
+                watched={watched}
+                onDeletWatched={handleDeleteWatched}
+              />
             </>
           )}
         </Box>

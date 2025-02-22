b3f469fad7e17d7657bb1f80a2e752ad87e69da7
Arlan Galvez <galvezalonso@gmail.com>
2025-02-22 14:20:00 -0500

    16-191: loading questions from a Fake API

--- 13-react-quiz/src/App.js
+++ 13-react-quiz/src/App.js
@@ -1,13 +1,50 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
-export default function App() {
-return <div className="app">
-<Header/>
+const initialState = {
+  status: "loading", // loading, error, ready, active, finished
+  questions: [],
+  error: null,
+};
 
-<Main>
-  <p>Content</p>
-  <p>More content</p>
-</Main>
-</div>
+
+function reducer(state, action) {
+  const {type,  payload } = action;
+  switch (type) {
+    case "dataReceived":
+      return { ...state, questions: payload, status: "ready"  };
+    case "dataFailed":
+      return { ...state, status: "error", error: payload };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { status, questions, error } = state; 
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((result) =>  result.json())
+      .then((resultData) => {
+        dispatch({ type: "dataReceived", payload: resultData });
+        // console.log(data);
+      })
+      .catch((err) => {
+        dispatch({ type: "dataFailed", payload: err.message });
+        // console.log(err.message);
+      });
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>questions: {questions.length}</p>
+        <p>Status: {status}</p>
+       {error && <p>Error content:  {error}</p>}
+      </Main>
+    </div>
+  );
 }

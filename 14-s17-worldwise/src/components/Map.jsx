c9d369a2df102311974f69a0755ed25676b9000b
Arlan Galvez <galvezalonso@gmail.com>
2025-03-15 12:34:16 -0400

    18-231: displayinf city markers on map

--- 14-s17-worldwise/src/components/Map.jsx
+++ 14-s17-worldwise/src/components/Map.jsx
@@ -1,31 +1,48 @@
 import { useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useCities } from "../contexts";
 import styles from "./Map.module.css";
 // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 
-
 function MapComponent() {
+  const { cities } = useCities();
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  const [mapPosition, setMapPosition] = useState([40,0])
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
-  const navigate = useNavigate()
   return (
-    <div className={styles.mapContainer} >
-      <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-    />
-    <Marker position={mapPosition}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-     
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            key={city.id}
+            position={[city.position.lat, city.position.lng]}
+            // eventHandlers={{
+            //   click: () => {
+            //     navigate(`/app/cities/${city.id}`);
+            //   },
+            // }}
+          >
+            
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }

4ce66373c236b6e9126facfbd25b68d5bf2e0e78
Arlan Galvez <galvezalonso@gmail.com>
2025-03-15 17:58:42 -0400

    18-240: adding route protection

---
+++ 14-s17-worldwise/src/pages/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts";
+
+export default function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/");
+  }, [isAuthenticated, navigate]);
+  return isAuthenticated ? children : null;
+}

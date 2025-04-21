f21bb1245c62c350803a978e16e8773555ea2ff6
Arlan Galvez <galvezalonso@gmail.com>
2025-04-20 20:00:03 -0400

    19-253: optimizin blunde size  with code splitting

--- 14-s17-worldwise/src/App.jsx
+++ 14-s17-worldwise/src/App.jsx
@@ -1,31 +1,52 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+import { AuthProvider, CitiesProvider } from "./contexts";
+import ProtectedRoute from "./pages/ProtectedRoute"; 
+
 import City from "./components/City";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import Form from "./components/Form";
-import { AuthProvider, CitiesProvider } from "./contexts";
-import AppLayout from "./pages/AppLayout";
-import HomePage from "./pages/HomePage";
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
-import Pricing from "./pages/Pricing";
-import Product from "./pages/Product";
-import ProtectedRoute from "./pages/ProtectedRoute";
 
+// PAGES
+// import AppLayout from "./pages/AppLayout";
+// import HomePage from "./pages/HomePage";
+// import Login from "./pages/Login";
+// import PageNotFound from "./pages/PageNotFound";
+// import Pricing from "./pages/Pricing";
+// import Product from "./pages/Product";
+
+// dist/assets/index-GViQfvm-.css   30.24 kB │ gzip:   5.08 kB
+// dist/assets/index-CVGo-FYo.js   566.52 kB │ gzip: 167.19 kB
+
+const AppLayout  = lazy(()=>import("./pages/AppLayout"))
+const HomePage  = lazy(()=>import("./pages/HomePage"))
+const Login  = lazy(()=>import("./pages/Login"))
+const PageNotFound  = lazy(()=>import("./pages/PageNotFound"))
+const Pricing  = lazy(()=>import("./pages/Pricing"))
+const Product  = lazy(()=>import("./pages/Product"))
 
 function App() {
-
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
+        <Suspense fallback={<SpinnerFullPage/>}>
           <Routes>
             <Route index element={<HomePage />} />
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<ProtectedRoute><AppLayout /></ProtectedRoute>}>
-              <Route index element={<Navigate to='cities' replace />} />
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate to="cities" replace />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
               <Route path="countries" element={<CountryList />} />
@@ -34,6 +55,7 @@ function App() {
             {/* this matches all paths that do not match the above routes */}
             <Route path="*" element={<PageNotFound />} />
           </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>

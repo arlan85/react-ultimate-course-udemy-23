9698e63632d18b14d38972832c9019bb08459bdf
Arlan Galvez <galvezalonso@gmail.com>
2026-04-18 12:04:08 -0400

    267: React! connect our redux app with react

--- 17-s20-262-redux-intro/src/index.js
+++ 17-s20-262-redux-intro/src/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import store from "./store";
 
-store.dispatch({ type: "account/deposit", payload: 500 }); // same as the useReducer function
-console.log("Hey Redux");
-console.log(store.getState());
-store.dispatch({ type: "account/withdraw", payload: 200 });
-console.log(store.getState());
+// store.dispatch({ type: "account/deposit", payload: 500 }); // same as the useReducer function
+// console.log("Hey Redux");
+// console.log(store.getState());
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );

5116f8bc5f4aad54189c7d8f37727a5746257851
Arlan Galvez <galvezalonso@gmail.com>
2026-05-07 17:40:04 -0400

    277: creating customer slice

--- 17-s20-262-redux-intro/src/features/customers/customerSlice.js
+++ 17-s20-262-redux-intro/src/features/customers/customerSlice.js
@@ -1,9 +1,44 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalId: "",
   createdAt: "",
 };
 
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer(state, action) {
+      /** // I've using objects as good practice but in case ther were separated params, we need to use the
+       * prepare function to return an object payload, or if we need to add other options like a random id or a date can be added
+       * prepare(fullName, nationalId) {
+       * return {
+       * payload: { fullName, nationalId createdAt: new Date().toISOString()},
+       * },
+       * //then use the reducer function to handle the logic of the reducer
+       * reduce(state, action) {
+       * state.fullName = action.payload.fullName;
+       * state.nationalId = action.payload.nationalId;
+       * state.createdAt = action.payload.createdAt;
+       * }
+       *
+       */
+      state.fullName = action.payload.fullName;
+      state.nationalId = action.payload.nationalId;
+      state.createdAt = new Date().toISOString();
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+export default customerSlice.reducer;
+
+/* V1 without redux toolkit
 // customer
 export function createCustomer(fullName, nationalId) {
   return {
@@ -49,4 +84,4 @@ export default function customerReducer(state = initialStateCustomer, action) {
     default:
       return state;
   }
-}
+} */

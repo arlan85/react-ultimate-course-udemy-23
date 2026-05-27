04db0048c59eb3d1b8345d3784494c10b6ef99dc
Arlan Galvez <galvezalonso@gmail.com>
2026-05-27 19:21:37 -0400

    289: handling errors with error elements

--- 18-s21-282-fast-react-pizza-co/src/App.jsx
+++ 18-s21-282-fast-react-pizza-co/src/App.jsx
@@ -1,21 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Cart from "./modules/cart/Cart";
-import Menu , {loader as menuLoader }from "./modules/menu/Menu"; //renamed for  improve readability
+import Menu, { loader as menuLoader } from "./modules/menu/Menu"; //renamed for  improve readability
 import CreateOrder from "./modules/order/CreateOrder";
 import Order from "./modules/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 import Home from "./ui/Home";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
         element: <Home />,
         // children: [], // nested routes go here
       },
-      { path: "/menu", element: <Menu />, loader: menuLoader }, //step 2 add the loader to the route
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      }, //step 2 add the loader to the route
       {
         path: "/cart",
         element: <Cart />,
@@ -28,10 +35,10 @@ const router = createBrowserRouter([
         path: "/order/new",
         element: <CreateOrder />,
       },
-      {
-        path: "*",
-        element: <h1>404 Not Found</h1>,
-      },
+      // {
+      //   path: "*",
+      //   element: <h1>404 Not Found</h1>,
+      // },
     ],
   },
 ]);

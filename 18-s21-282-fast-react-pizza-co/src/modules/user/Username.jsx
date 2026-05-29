c87445e4ab3f43e9e9e9365f683beff3444a802c
Arlan Galvez <galvezalonso@gmail.com>
2026-05-29 15:00:49 -0400

    300: using flexbox

--- 18-s21-282-fast-react-pizza-co/src/modules/user/Username.jsx
+++ 18-s21-282-fast-react-pizza-co/src/modules/user/Username.jsx
@@ -1,6 +1,6 @@
 function Username() {
   return (
-    <div className="text-sm font-semibold py-2 hidden" >
+    <div className="text-sm font-semibold py-2 hidden md:block" >
       Arlan
     </div>
   )

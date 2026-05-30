952e0bb330398e6bca8ac763584bba0a164e44f5
Arlan Galvez <galvezalonso@gmail.com>
2026-05-30 02:07:02 -0400

    307: custom font family

--- 18-s21-282-fast-react-pizza-co/src/ui/Header.jsx
+++ 18-s21-282-fast-react-pizza-co/src/ui/Header.jsx
@@ -1,17 +1,18 @@
-import { Link } from "react-router-dom"
-import SearchOrder from "../modules/order/SearchOrder"
-import Username from "../modules/user/Username"
+import { Link } from 'react-router-dom';
+import SearchOrder from '../modules/order/SearchOrder';
+import Username from '../modules/user/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-500 p-4 uppercase border-b-4 border-stone-300 sm:px-6 flex items-center justify-around">
+    <header className="flex items-center justify-around border-b-4
+     border-stone-300 bg-yellow-500 p-4 font-serif uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast Pizza Co.
       </Link>
-      <SearchOrder/>
-      <Username/>
+      <SearchOrder />
+      <Username />
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- 18-s21-282-fast-react-pizza-co/tailwind.config.js
+++ 18-s21-282-fast-react-pizza-co/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // needs to be changed if the src changes
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], // needs to be changed if the src changes
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // pizza: ['Roboto Mono', 'monospace'], // for an specific place for fonts
+        sans: ['Roboto Mono', 'monospace'], // for the whole site
+      },
+      colors: {
+        pizza: '#123456',
+      },
+      fontSize: {
+        huge: ['80rem', { lineHeight: '1' }],
+      },
+      height:{
+        screen: '100dvh'
+      }
+    },
   },
   plugins: [],
 };

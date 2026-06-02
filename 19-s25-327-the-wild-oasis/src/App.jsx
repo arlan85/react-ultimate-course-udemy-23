a47b875649756c85f153cb3efe1da9c9fc6f5e7d
Arlan Galvez <galvezalonso@gmail.com>
2026-06-02 15:22:48 -0400

    332: building more reusable styled components

--- 19-s25-327-the-wild-oasis/src/App.jsx
+++ 19-s25-327-the-wild-oasis/src/App.jsx
@@ -1,26 +1,40 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
-import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Input from "./ui/Input";
+import Row from "./ui/Row";
 
-const StyledApp = styled.div` //for the root styled component od the app
-  background-color: orange;
+const StyledApp = styled.div`
+  //for the root styled component od the app
+  /* background-color: orange; */
   padding: 20px;
-`
+`;
 
 function App() {
-  return (<>
-  <GlobalStyles/>
-    <StyledApp>
-      <Heading as='h1'>The wild Oasis</Heading>
-      <Heading as='h2'>The wild Oasis</Heading>
-      <Button onClick={()=>alert("Alert On")}>Check in</Button>
-      <Button onClick={()=>alert("Alert On")}>Check out</Button>
-      <Heading as='h3'>The wild Oasis</Heading>
-      <Input type="number" placeholder="number of guests"/>
-      <Input type="number" placeholder="number of guests"/>
-    </StyledApp></>
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">The wild Oasis</Heading>
+            <div>
+              <Heading as="h2">The wild Oasis</Heading>
+              <Button variation="primary" size="medium" onClick={() => alert("Alert On")}>Check in</Button>
+              <Button variation="secondary" size="small" onClick={() => alert("Alert On")}>Check out</Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="number of guests" />
+              <Input type="number" placeholder="number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
   );
 }
 
---
+++ 19-s25-327-the-wild-oasis/src/ui/Row.jsx
@@ -0,0 +1,24 @@
+import styled, { css } from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+  ${(props) =>
+    props.type === "horizontal" &&
+    css`
+      justify-content: space-between;
+      align-items: center;
+    `}
+
+  ${(props) =>
+    props.type === "vertical" &&
+    css`
+      flex-direction: column;
+      gap: 1.6rem;
+    `}
+`;
+
+Row.defaultProps = { // you can define defaul props for the React components 
+  type: "vertical",
+};
+
+export default Row;

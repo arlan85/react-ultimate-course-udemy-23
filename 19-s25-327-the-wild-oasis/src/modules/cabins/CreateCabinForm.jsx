e7e50c0956e04dbfede9352d51a23e6983f29457
Arlan Galvez <galvezalonso@gmail.com>
2026-06-03 23:47:01 -0400

    356: editing a cabin

--- 19-s25-327-the-wild-oasis/src/modules/cabins/CreateCabinForm.jsx
+++ 19-s25-327-the-wild-oasis/src/modules/cabins/CreateCabinForm.jsx
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 
-import { createCabin } from "../../services/apiCabins";
+import { createEditCabin } from "../../services/apiCabins";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Form from "../../ui/Form";
@@ -10,14 +10,26 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Textarea from "../../ui/Textarea";
 
-function CreateCabinForm() {
-  const { register, handleSubmit, reset, getValues, formState } = useForm();
+function CreateCabinForm({ cabinToEdit = {} }) {
+  const { id: editId, ...editValues } = cabinToEdit;
+
+  const isEditSession = Boolean(editId);
+
+  const { register, handleSubmit, reset, getValues, formState } = useForm({
+    defaultValues: isEditSession
+      ? editValues
+      : {
+          maxCapacity: 1,
+          regularPrice: 0,
+          discount: 0,
+        },
+  });
   const { errors } = formState;
   const queryClient = useQueryClient();
 
-  const { mutate, isPending: isCreating } = useMutation({
+  const { mutate: createCabin, isPending: isCreating } = useMutation({
     //encapsulet the changes were the mutation is happening
-    mutationFn: createCabin,
+    mutationFn: (newCabinData) => createEditCabin(newCabinData),
     onSuccess: () => {
       toast.success("New cabin successfully created!");
       queryClient.invalidateQueries({
@@ -28,8 +40,26 @@ function CreateCabinForm() {
     onError: (err) => toast.error(err.message),
   });
 
-  function hangleCreatCabin(data) {
-    mutate({...data, image: data.image[0]});
+  const { mutate: editCabin, isPending: isEditing } = useMutation({
+    //encapsulet the changes were the mutation is happening
+    mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id), //only accepts one param (object)
+    onSuccess: () => {
+      toast.success("Cabin successfully Edited!");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  const inProgress = isCreating || isEditing;
+
+  function handleCreateCabin(data) {
+    const image = typeof data.image === "string" ? data.image : data.image[0];
+    if (isEditSession)
+      editCabin({ newCabinData: { ...data, image }, id: editId });
+    else createCabin({ ...data, image });
   }
 
   // to know that exsists but errors are hadled by the const { errors } = formState; from useForm hook
@@ -38,11 +68,12 @@ function CreateCabinForm() {
   }
 
   return (
-    <Form onSubmit={handleSubmit(hangleCreatCabin, onError)}>
+    <Form onSubmit={handleSubmit(handleCreateCabin, onError)}>
       <FormRow label=" Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
           id="name"
+          disabled={inProgress}
           {...register("name", {
             required: "this field is required",
           })}
@@ -52,8 +83,10 @@ function CreateCabinForm() {
         <Input
           type="number"
           id="maxCapacity"
+          disabled={inProgress}
           {...register("maxCapacity", {
             required: "this field is required",
+            valueAsNumber: true,
             min: {
               value: 1,
               message: "Capacity must be at least 1",
@@ -66,8 +99,10 @@ function CreateCabinForm() {
         <Input
           type="number"
           id="regularPrice"
+          disabled={inProgress}
           {...register("regularPrice", {
             required: "this field is required",
+            valueAsNumber: true,
             min: {
               value: 1,
               message: "Regular Price must be at least 1",
@@ -80,11 +115,13 @@ function CreateCabinForm() {
         <Input
           type="number"
           id="discount"
+          disabled={inProgress}
           defaultValue={0}
           {...register("discount", {
             required: "this field is required",
+            valueAsNumber: true,
             validate: (value) =>
-              value <= getValues().regularPrice ||
+              Number(value) <= Number(getValues().regularPrice) ||
               "Discount must be less that regular price",
           })}
         />
@@ -97,6 +134,7 @@ function CreateCabinForm() {
         <Textarea
           type="number"
           id="description"
+          disabled={inProgress}
           defaultValue=""
           {...register("description", {
             required: "this field is required",
@@ -107,20 +145,25 @@ function CreateCabinForm() {
       <FormRow label="Cabin photo" error={errors?.image?.message}>
         <FileInput
           id="image"
+          disabled={inProgress}
           accept="image/*"
           type="file"
           {...register("image", {
-            required: "this field is required",
+            required: isEditSession ? false : "this field is required",
           })}
         />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
+        {!isEditSession && (
+          <Button variation="secondary" type="reset">
+            Cancel
+          </Button>
+        )}
+        <Button disabled={inProgress}>
+          {isEditSession ? "Edit" : "Create new"} cabin
         </Button>
-        <Button disabled={isCreating}>Edit cabin</Button>
       </FormRow>
     </Form>
   );
--- 19-s25-327-the-wild-oasis/src/services/apiCabins.js
+++ 19-s25-327-the-wild-oasis/src/services/apiCabins.js
@@ -9,35 +9,51 @@ export async function getCabins() {
   return data;
 }
 
-export async function createCabin(newCabin) {
-  const imageName = `${Math.random()}-$${newCabin.image.name}`.replaceAll(
-    "/",
-    "",
-  );
+export async function createEditCabin(newCabin, id) {
   const supabaseBaseUrl = supabaseUrl || "http://127.0.0.1:54341";
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseBaseUrl); //we already have the image in supabase do not require to de upload.
 
-  const imagePath = `${supabaseBaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  console.log("newCabin", newCabin);
+  const imageName = hasImagePath
+    ? ""
+    : `${new Date().getTime()}-${newCabin.image.name}`.replaceAll("/", "");
 
-  const { data, error } = await supabase
-    .from("cabins")
-    .insert([{ ...newCabin, image: imagePath }]); // this works because names matches with table cols
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseBaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  let query = supabase.from("cabins");
+
+  if (!id) {
+    const insertPayload = { ...newCabin, image: imagePath };
+    console.log("INSERT PAYLOAD", JSON.stringify(insertPayload, null, 2));
+    query = query.insert([insertPayload]); // this works because names matches with table cols
+  }
+  if (id) {
+    query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
+  }
+
+  const { data, error } = await query.select().single();
+
   if (error) {
     console.log(error);
     throw new Error("Cabin could not be added");
   }
 
   /** 2 upload image */
-  // http://127.0.0.1:54341/storage/v1/object/public/cabin-images/cabin-001.jpg
-  const { error: imageError } = await supabase.storage
-    .from("cabin-images")
-    .upload(imageName, newCabin.image);
-  if (imageError) {
-    //3  Handle error
-    await supabase.from("cabins").delete().eq("id", data.id);
-    console.error(imageError);
-    throw new Error(
-      "Cabin image could\`t be uploaded, the cabin was not created.",
-    );
+  if (!hasImagePath) {
+    // http://127.0.0.1:54341/storage/v1/object/public/cabin-images/cabin-001.jpg
+    const { error: imageError } = await supabase.storage
+      .from("cabin-images")
+      .upload(imageName, newCabin.image);
+    if (imageError) {
+      //3  Handle error
+      await supabase.from("cabins").delete().eq("id", data.id);
+      console.error(imageError);
+      throw new Error(
+        "Cabin image could`t be uploaded, the cabin was not created.",
+      );
+    }
   }
   return data;
 }

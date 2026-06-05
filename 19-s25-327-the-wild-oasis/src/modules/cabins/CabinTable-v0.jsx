08eb9ea0e9ca6e92a3a6812edbdc9509a67bf860
Arlan Galvez <galvezalonso@gmail.com>
2026-06-05 18:15:32 -0400

    371: building a reusable table

--- 19-s25-327-the-wild-oasis/src/modules/cabins/CabinTable-v0.jsx
+++ 19-s25-327-the-wild-oasis/src/modules/cabins/CabinTable-v0.jsx
@@ -4,7 +4,15 @@ import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
-import Table from "../../ui/Table";
+
+const Table = styled.div`
+  border: 1px solid var(--color-grey-200);
+
+  font-size: 1.4rem;
+  background-color: var(--color-grey-0);
+  border-radius: 7px;
+  overflow: hidden;
+`;
 
 const TableHeader = styled.header`
   display: grid;
@@ -29,15 +37,15 @@ function CabinTable() {
   if (isPending) return <Spinner />;
 
   return (
-    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-      <Table.Header role="row">
+    <Table tole="table">
+      <TableHeader role="row">
         <div></div>
         <div>Cabin</div>
         <div>Capacity</div>
         <div>Price</div>
         <div>Discount</div>
         <div></div>
-      </Table.Header>
+      </TableHeader>
       {cabins?.map((cabin) => (
         <CabinRow key={cabin.id} cabin={cabin} />
       ))}

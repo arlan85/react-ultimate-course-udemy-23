9de0096cecbdc9c89a5d479561d495449cb8653d
Arlan Galvez <galvezalonso@gmail.com>
2026-06-08 02:27:19 -0400

    377: building the bookings table

--- 19-s25-327-the-wild-oasis/src/pages/Bookings.jsx
+++ 19-s25-327-the-wild-oasis/src/pages/Bookings.jsx
@@ -1,12 +1,16 @@
+import BookingTable from "../modules/bookings/BookingTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Bookings() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All bookings</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All bookings</Heading>
+        <p>TEST</p>
+      </Row>
+      <BookingTable></BookingTable>
+    </>
   );
 }
 
